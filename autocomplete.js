a81748b11d3b0a1bc118afbbb359f259d656bc21
pranshulrohila <pranshul.ruhela@gmail.com>
2023-04-17 19:06:53 +0530

    Added Dynamic Dropdown

--- autocomplete.js
+++ autocomplete.js
@@ -1,4 +1,3 @@
-// https://restcountries.com/v3.1/all
 
 const inputEl = document.querySelector("#country-input");
 inputEl.addEventListener("input", onInputChange);
@@ -24,7 +23,6 @@ function onInputChange() {
     const filteredNames = [];
     countryCommonNames.forEach((countryName)=>{
         if (countryName.toLowerCase().includes(value)) {
-        // if(countryName.substr(0, value.length).toLowerCase() === value) {
             filteredNames.push(countryName);
         }
     });
@@ -56,4 +54,85 @@ function onCountryButtonClick(e) {
     const buttonEl = e.target;
     inputEl.value=buttonEl.innerHTML;
     removeAutocompleteDropdown();
+}
+// Define the options for each dropdown
+var regionOptions = {
+    AMRS: ['FCMCMS'],
+    APAC: ['MLEA', 'MLAF', 'MLFE', 'MLSSEC', 'MLSFO'],
+    EMEA: ['Normal', 'Alternative', 'ECMS Normal', 'ECMS Exception']
+};
+
+var entityOptions = {
+    AMRS: {
+        FCMCMS: ['Secured', 'Segregated', 'Segregated Cross', 'Cleaed Swaps']
+    },
+    APAC: {
+        MLEA: [],
+        MLAF: [],
+        MLFE: ['HKCC', 'SEOCH'],
+        MLSSEC: ['Asset', 'CDP', 'NCDP'],
+        MLSFO: ['Asset', 'Cash']
+    },
+    EMEA: {
+        Normal: [],
+        Alternative: [],
+        'ECMS Normal': [],
+        'ECMS Exception': []
+    }
+};
+
+// Function to populate entity based on the selected option of region
+function populateEntity() {
+    var region = document.getElementById("region");
+    var entity = document.getElementById("entity");
+    var comp = document.getElementById("comp");
+    entity.innerHTML = ""; // Clear entity options
+    comp.innerHTML = ""; // Clear comp options
+    comp.disabled = true;
+    var selectedRegion = region.value;
+    if (selectedRegion !== "") {
+        var options = regionOptions[selectedRegion];
+        for (var i = 0; i < options.length; i++) {
+            var option = document.createElement("option");
+            option.text = options[i];
+            option.innerHTML = options[i];
+            entity.add(option);
+        }
+        if (options.length === 0) {
+            // Disable entity if there are no suboptions
+            entity.disabled = true;
+        } else {
+            // Enable comp if there are suboptions
+            entity.disabled = false;
+        }
+    } else {
+        entity.disabled = true;
+        comp.disabled = true;
+    }
+}
+
+// Function to populate comp based on the selected options of region and entity
+function populateComp() {
+    var region = document.getElementById("region");
+    var entity = document.getElementById("entity");
+    var comp = document.getElementById("comp");
+    comp.innerHTML = ""; // Clear comp options
+    var region = region.value;
+    var entity = entity.value;
+    if (region !== "" && entity !== "") {
+        var compOptions = entityOptions[region][entity];
+        if (compOptions.length === 0) {
+            comp.disabled = true;
+        } else {
+            comp.disabled = false;
+            for (var i = 0; i < compOptions.length; i++) {
+                var option = document.createElement("option");
+                option.text = compOptions[i];
+                option.innerHTML = compOptions[i];
+                comp.add(option);
+            }
+        }
+    } else {
+        comp.disabled = true;
+    }
 }
